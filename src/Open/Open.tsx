b0d853de453615c442f45ff4636e2dafa9f69d3b
koo3701 <koo.0107.koo@gmail.com>
2022-09-05 02:44:03 +0900

    add fullscreen link

--- src/Open/Open.tsx
+++ src/Open/Open.tsx
@@ -8,17 +8,19 @@ import { Container } from 'reactstrap';
 export const Open: React.FC = () => {
   const location = useLocation();
   const uri = new URL(window.location.href);
+  const redirectUrl = `https://www.youtube.com/redirect?q=${uri.protocol}//${uri.host}/`;
 
-  if (location.state !== true)
-    window.location.replace(
-      `https://www.youtube.com/redirect?q=${uri.protocol}//${uri.host}/`
-    );
+  if (location.state !== true) window.location.replace(redirectUrl);
+
   return location.state === true ? (
     <Container className='d-flex align-items-center justify-content-center vw-100 vh-100 text-center'>
       <div>
         {' '}
         <h2>This is Bookmark Mode!</h2>
         <h5>Add This Page to Your Tesla Browser Bookmark!</h5>
+        <p>
+          or <a href={redirectUrl}>Open Fullscreen Mode</a>
+        </p>
       </div>
     </Container>
   ) : (
